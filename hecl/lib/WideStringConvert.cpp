199b8e7c321032d63434f9aecd718abfbb4c7970
Jack Andersen <jackoalan@gmail.com>
2015-08-24 21:02:10 -1000

    Removed codecvt

--- hecl/lib/WideStringConvert.cpp
+++ hecl/lib/WideStringConvert.cpp
@@ -1,20 +1,33 @@
 #include "HECL/HECL.hpp"
-#include <locale>
-#include <codecvt>
 
 namespace HECL
 {
 
 std::string WideToUTF8(const std::wstring& src)
 {
-    std::wstring_convert<std::codecvt_utf8<wchar_t>> conv;
-    return conv.to_bytes(src);
+    std::string retval;
+    retval.reserve(src.length());
+    for (wchar_t ch : src)
+    {
+        char mb[4];
+        int c = std::wctomb(mb, ch);
+        retval.append(mb, c);
+    }
+    return retval;
 }
 
 std::wstring UTF8ToWide(const std::string& src)
 {
-    std::wstring_convert<std::codecvt_utf8<wchar_t>> conv;
-    return conv.from_bytes(src);
+    std::wstring retval;
+    retval.reserve(src.length());
+    const char* buf = src.c_str();
+    while (*buf)
+    {
+        wchar_t wc;
+        buf += std::mbtowc(&wc, buf, MB_CUR_MAX);
+        retval += wc;
+    }
+    return retval;
 }
 
 }
