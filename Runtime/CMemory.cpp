e9fdb667d80e8f13f85413273e9e7e2cc21b7322
Phillip Stephens <antidote.crk@gmail.com>
2015-09-08 01:39:57 -0700

    CGameAllocator fixes

--- Runtime/CMemory.cpp
+++ Runtime/CMemory.cpp
@@ -75,7 +75,6 @@ void* operator new(std::size_t sz)
 {
     if (!Retro::g_memoryAllocatorReady)
         return malloc(sz);
-
     Retro::CCallStack cs("?\?(?\?)", "UnknownType");
     return Retro::CMemory::Alloc(sz,
                                  Retro::IAllocator::EHint::None,
@@ -104,6 +103,7 @@ void operator delete(void* ptr) noexcept
         free(ptr);
         return;
     }
+
     Retro::CMemory::Free(ptr);
 }
 
@@ -140,5 +140,6 @@ void operator delete[](void* ptr) noexcept
         free(ptr);
         return;
     }
+
     Retro::CMemory::Free(ptr);
 }
