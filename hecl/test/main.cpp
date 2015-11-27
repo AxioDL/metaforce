89077c3233bb9a8a2cf515d2ea0d59e338ea472c
Jack Andersen <jackoalan@gmail.com>
2015-11-27 12:17:09 -1000

    Update submodules

--- hecl/test/main.cpp
+++ hecl/test/main.cpp
@@ -188,6 +188,7 @@ struct HECLApplicationCallback : boo::IApplicationCallback
             r.location[0] = 0;
             r.location[1] = 0;
             gfxQ->setViewport(r);
+            gfxQ->setScissor(r);
             float rgba[] = {sinf(frameIdx / 60.0), cosf(frameIdx / 60.0), 0.0, 1.0};
             gfxQ->setClearColor(rgba);
             gfxQ->clearTarget();
