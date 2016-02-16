b647a0a54323c7993c48933386c8487a0853beac
Phillip Stephens <antidote.crk@gmail.com>
2016-02-15 23:09:57 -0800

    Add found resources to pool

--- Runtime/CSimplePool.cpp
+++ Runtime/CSimplePool.cpp
@@ -22,8 +22,12 @@ CToken CSimplePool::GetObj(const SObjectTag& tag, const CVParamTransfer& paramXf
     // TODO: There is some logic missing here, need to figure out what it's doing
     CObjectReference* ret = new CObjectReference(*this, x30_factory.Build(tag, paramXfer), tag, paramXfer);
     if (ret->GetObject())
+    {
+        x4_resources.push_back(std::make_pair<SObjectTag, CObjectReference*>((SObjectTag)tag, std::move(ret)));
         return CToken(ret);
+    }
 
+    delete ret;
     return CToken();
 }
 
