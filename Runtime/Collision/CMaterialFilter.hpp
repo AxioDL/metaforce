7b2705db52169dfeefd7f1dd572848c1e7ff3e85
Phillip Stephens <antidote.crk@gmail.com>
2016-04-25 00:58:12 -0700

    Update specter

--- Runtime/Collision/CMaterialFilter.hpp
+++ Runtime/Collision/CMaterialFilter.hpp
@@ -16,33 +16,19 @@ public:
         Three
     };
 private:
-
     CMaterialList x0_include;
     CMaterialList x8_exclude;
     EFilterType x10_type = EFilterType::Three;
 public:
+    static const CMaterialFilter skPassEverything;
 
-    CMaterialFilter(const CMaterialList& include, const CMaterialList& exclude, EFilterType type)
-        : x0_include(include),
-          x8_exclude(exclude),
-          x10_type(type)
-    {
-    }
+    CMaterialFilter(const CMaterialList& include, const CMaterialList& exclude, EFilterType type);
 
-    static CMaterialFilter MakeInclude(const CMaterialList& include)
-    {
-        return CMaterialFilter(include, {EMaterialTypes::Zero}, EFilterType::Zero);
-    }
+    static CMaterialFilter MakeInclude(const CMaterialList& include);
 
-    static CMaterialFilter MakeExclude(const CMaterialList& exclude)
-    {
-        return CMaterialFilter({u64(0x00000000FFFFFFFF)}, exclude, EFilterType::Two);
-    }
+    static CMaterialFilter MakeExclude(const CMaterialList& exclude);
 
-    static CMaterialFilter MakeIncludeExclude(const CMaterialList& include, const CMaterialList& exclude)
-    {
-        return CMaterialFilter(include, exclude, EFilterType::Three);
-    }
+    static CMaterialFilter MakeIncludeExclude(const CMaterialList& include, const CMaterialList& exclude);
 
     const CMaterialList& GetIncludeList() const { return x0_include; }
     const CMaterialList& GetExcludeList() const { return x8_exclude; }
