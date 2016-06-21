54ec899d72822c3b904a8559dc2ab0f869594d78
Phillip Stephens <antidote.crk@gmail.com>
2016-06-21 00:11:49 -0700

    Add missing files

---
+++ Runtime/MP1/CSpacePirate.cpp
@@ -0,0 +1,15 @@
+#include "CSpacePirate.hpp"
+
+namespace urde
+{
+namespace MP1
+{
+CSpacePirate::CSpacePirate(TUniqueId uid, const std::string& name, const CEntityInfo& info, const zeus::CTransform& xf,
+                           CModelData&& mData, const CActorParameters& aParams, const CPatternedInfo& pInfo, CInputStream& in,
+                           u32 propCount)
+    : CPatterned(EUnknown::ThirtyFour, uid, name, EFlavorType::Zero, info, xf, std::move(mData), pInfo, EMovementType::Ground,
+                 EColliderType::One, EBodyType::One, aParams, true)
+{
+}
+}
+}
---
+++ Runtime/MP1/CSpacePirate.hpp
@@ -0,0 +1,18 @@
+#ifndef __URDE_MP1_CSPACEPIRATE_HPP__
+#define __URDE_MP1_CSPACEPIRATE_HPP__
+
+#include "World/CPatterned.hpp"
+
+namespace urde
+{
+namespace MP1
+{
+class CSpacePirate : public CPatterned
+{
+public:
+    CSpacePirate(TUniqueId, const std::string&, const CEntityInfo&, const zeus::CTransform&, CModelData&&,
+                 const CActorParameters&, const CPatternedInfo&, CInputStream&, u32);
+};
+}
+}
+#endif // __URDE_MP1_CSPACEPIRATE_HPP__
