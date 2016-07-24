75a7e35f8c6d6dd7a8c796d5dcaf7ce890603de7
Phillip Stephens <antidote.crk@gmail.com>
2016-07-24 00:39:58 -0700

    Initial CSaveWorld imps

--- Runtime/CSaveWorld.hpp
+++ Runtime/CSaveWorld.hpp
@@ -2,12 +2,43 @@
 #define __URDE_CSAVEWORLD_HPP__
 
 #include "RetroTypes.hpp"
+#include "DNACommon/SAVWCommon.hpp"
 
 namespace urde
 {
+
 class CSaveWorld
 {
+public:
+    using EScanCategory = DataSpec::SAVWCommon::EScanCategory;
+    struct SScanState
+    {
+        ResId x0_id;
+        EScanCategory x4_category;
+    };
+
+    struct SLayerState
+    {
+        TAreaId x0_area;
+        u32 x4_layer;
+    };
+
+private:
+    u32 x0_areaCount;
+    std::vector<TEditorId> x4_cinematics;
+    std::vector<TEditorId> x14_relays;
+    std::vector<SLayerState> x24_layers;
+    std::vector<TEditorId> x34_doors;
+    std::vector<SScanState> x44_scans;
+public:
     CSaveWorld(CInputStream& in);
+    u32 GetAreaCount() const;
+    u32 GetCinematicCount() const;
+    s32 GetCinematicIndex(const TEditorId& id) const;
+    u32 GetRelayCount() const;
+    s32 GetRelayIndex(const TEditorId& id) const;
+    u32 GetDoorCount() const;
+    s32 GetDoorIndex(const TEditorId &id) const;
 };
 }
 
