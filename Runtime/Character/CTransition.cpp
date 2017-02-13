242f9e3a2dec5c4c77df25774bfd342df5892257
Phillip Stephens <antidote.crk@gmail.com>
2017-02-12 19:19:28 -0800

    Implement CTransition constructor

--- Runtime/Character/CTransition.cpp
+++ Runtime/Character/CTransition.cpp
@@ -4,6 +4,10 @@ namespace urde
 {
 
 CTransition::CTransition(CInputStream& in)
+    : x0_id(in.readUint32Big())
+    , x4_animA(in.readUint32Big())
+    , x8_animB(in.readUint32Big())
+    , xc_trans(CMetaTransFactory::CreateMetaTrans(in))
 {
 }
 
