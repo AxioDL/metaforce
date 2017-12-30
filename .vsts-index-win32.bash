252c136536ed77863eb56f02ba07e106afbad8ff
Jack Andersen <jackoalan@gmail.com>
2017-12-30 00:08:34 -1000

    Correct filenames for win32 index script

--- .vsts-index-win32.bash
+++ .vsts-index-win32.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo urde-$1-win32-$2-sse2.exe > $3
-echo urde-$1-win32-$2-sse41.exe >> $3
-echo urde-$1-win32-$2-avx.exe >> $3
-echo urde-$1-win32-$2-avx2.exe >> $3
+echo urde-$1-win32-$2-sse2.zip > $3
+echo urde-$1-win32-$2-sse41.zip >> $3
+echo urde-$1-win32-$2-avx.zip >> $3
+echo urde-$1-win32-$2-avx2.zip >> $3
