2004c974808fa7091bda5b7e2ec920b05678046e
Phillip Stephens <antidote.crk@gmail.com>
2018-01-21 18:45:41 -0800

    Restore old CVar system

--- DataSpec/DNAMP1/Tweaks/CTweakGame.cpp
+++ DataSpec/DNAMP1/Tweaks/CTweakGame.cpp
@@ -5,7 +5,7 @@ namespace DataSpec::DNAMP1
 {
 void CTweakGame::initCVars(hecl::CVarManager* mgr)
 {
-    mgr->findOrMakeCVar("tweaks.game.hardmodeDamageMult", "", x60_hardmodeDamageMult, hecl::CVar::EFlags::Game | hecl::CVar::Archive | hecl::CVar::Cheat);
-    mgr->findOrMakeCVar("tweaks.game.hardmodeWeaponMult", "", x64_hardmodeWeaponMult, hecl::CVar::EFlags::Game | hecl::CVar::Archive | hecl::CVar::Cheat);
+    //mgr->findOrMakeCVar("tweaks.game.hardmodeDamageMult", "", x60_hardmodeDamageMult, hecl::CVar::EFlags::Game | hecl::CVar::Archive | hecl::CVar::Cheat);
+    //mgr->findOrMakeCVar("tweaks.game.hardmodeWeaponMult", "", x64_hardmodeWeaponMult, hecl::CVar::EFlags::Game | hecl::CVar::Archive | hecl::CVar::Cheat);
 }
 }
