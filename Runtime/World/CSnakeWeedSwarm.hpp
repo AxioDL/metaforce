e116d887c72665622e2d612061c915141ea1991f
Phillip Stephens <antidote.crk@gmail.com>
2018-06-23 16:08:16 -0700

    Implement CSnakeWeedSwarm loader

--- Runtime/World/CSnakeWeedSwarm.hpp
+++ Runtime/World/CSnakeWeedSwarm.hpp
@@ -6,9 +6,16 @@
 
 namespace urde
 {
+class CAnimationParameters;
 class CSnakeWeedSwarm : public CActor
 {
 public:
+    CSnakeWeedSwarm(TUniqueId, bool, std::string_view, const CEntityInfo&, const zeus::CVector3f&, const zeus::CVector3f&,
+                    const CAnimationParameters&, const CActorParameters&, float, float, float, float, float, float, float,
+                    float, float, float, float, float, float, float, const CDamageInfo&, float, u32, u32, u32, u32, u32,
+                    u32, float);
+
+    void Accept(IVisitor&);
     void ApplyRadiusDamage(const zeus::CVector3f& pos, const CDamageInfo& info,
                            CStateManager& stateMgr) {}
 };
