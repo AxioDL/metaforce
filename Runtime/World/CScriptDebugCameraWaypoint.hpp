134f5a68a23f783fbe2222d3d2cd135f086d1034
Phillip Stephens <antidote.crk@gmail.com>
2018-10-30 15:21:29 -0700

    Implement CScriptDebugCameraWaypoint loader

--- Runtime/World/CScriptDebugCameraWaypoint.hpp
+++ Runtime/World/CScriptDebugCameraWaypoint.hpp
@@ -11,6 +11,8 @@ class CScriptDebugCameraWaypoint : public CActor
 public:
     CScriptDebugCameraWaypoint(TUniqueId uid, std::string_view name, const CEntityInfo& info,
                                const zeus::CTransform& xf, u32 w1);
+
+    void Accept(IVisitor&);
 };
 
 }
