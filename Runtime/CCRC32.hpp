f430a003149e7bf1b2181871d02d0cafd6c57501
Lioncash <mathew1800@gmail.com>
2019-09-08 18:18:26 -0400

    CCRC32: Move table declaration into the cpp file
    This can be moved entirely into the cpp file, making it an
    implementation detail.

--- Runtime/CCRC32.hpp
+++ Runtime/CCRC32.hpp
@@ -1,14 +1,10 @@
 #pragma once
+
 #include <cstdint>
 
 namespace urde {
 
 class CCRC32 {
-  static const uint32_t crc32Table[256];
-  static uint32_t permute(uint32_t checksum, uint8_t b) {
-    return (checksum >> 8) ^ crc32Table[(checksum & 0xFF) ^ b];
-  }
-
 public:
   static uint32_t Calculate(const void* data, uint32_t length);
 };
