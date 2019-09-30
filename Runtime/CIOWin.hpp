99a7e64a8a941170361d13b15216f95218aa85af
Lioncash <mathew1800@gmail.com>
2019-09-30 18:18:53 -0400

    CIOWin: Default the virtual destructor
    No functional change. Just maintains consistency with the rest of the
    codebase.

--- Runtime/CIOWin.hpp
+++ Runtime/CIOWin.hpp
@@ -16,12 +16,14 @@ class CIOWin {
 
 public:
   enum class EMessageReturn { Normal = 0, Exit = 1, RemoveIOWinAndExit = 2, RemoveIOWin = 3 };
-  virtual ~CIOWin() {}
   CIOWin(std::string_view name) : x4_name(name) { m_nameHash = std::hash<std::string_view>()(name); }
+
+  virtual ~CIOWin() = default;
   virtual EMessageReturn OnMessage(const CArchitectureMessage&, CArchitectureQueue&) = 0;
   virtual bool GetIsContinueDraw() const { return true; }
   virtual void Draw() const {}
   virtual void PreDraw() const {}
+
   std::string_view GetName() const { return x4_name; }
   size_t GetNameHash() const { return m_nameHash; }
 };
