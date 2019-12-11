4e5921c952ab05bcb4c4612c3ef69a753dae3a25
Phillip Stephens <antidote.crk@gmail.com>
2019-12-10 20:51:23 -0800

    Update submodules

---
+++ Runtime/CTextureCache.cpp
@@ -0,0 +1,5 @@
+//
+// Created by antidote on 12/3/19.
+//
+
+#include "CTextureCache.hpp"
---
+++ Runtime/CTextureCache.hpp
@@ -0,0 +1,10 @@
+//
+// Created by antidote on 12/3/19.
+//
+
+#ifndef URDE_CTEXTURECACHE_HPP
+#define URDE_CTEXTURECACHE_HPP
+
+class CTextureCache {};
+
+#endif // URDE_CTEXTURECACHE_HPP
