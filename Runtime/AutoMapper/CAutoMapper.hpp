50fbd2ad42b459dd42d3f7aac6e8770f955d974d
Lioncash <mathew1800@gmail.com>
2020-01-28 13:26:48 -0500

    CAutoMapper: Make SAutoMapperRenderState constructor parameter names more informative

--- Runtime/AutoMapper/CAutoMapper.hpp
+++ Runtime/AutoMapper/CAutoMapper.hpp
@@ -31,7 +31,8 @@ public:
   enum class EAutoMapperState { MiniMap, MapScreen, MapScreenUniverse };
   struct SAutoMapperRenderState {
     enum class Ease { None, Linear, Out, In, InOut };
-    typedef zeus::CVector2i (*FGetViewportSize)();
+    using FGetViewportSize = zeus::CVector2i (*)();
+
     FGetViewportSize m_getViewportSize;
     zeus::CVector2i x0_viewportSize;
     zeus::CQuaternion x8_camOrientation;
@@ -51,20 +52,22 @@ public:
     Ease x54_depth2Ease;
     Ease x58_alphaEase;
     SAutoMapperRenderState() = default;
-    SAutoMapperRenderState(FGetViewportSize v1, const zeus::CQuaternion& rot, float f1, float f2,
-                           const zeus::CVector3f& v2, float f3, float f4, float f5, float f6, float f7, float f8)
-    : m_getViewportSize(v1)
-    , x0_viewportSize(v1())
-    , x8_camOrientation(rot)
-    , x18_camDist(f1)
-    , x1c_camAngle(f2)
-    , x20_areaPoint(v2)
-    , x2c_drawDepth1(f3)
-    , x30_drawDepth2(f4)
-    , x34_alphaSurfaceVisited(f5)
-    , x38_alphaOutlineVisited(f6)
-    , x3c_alphaSurfaceUnvisited(f7)
-    , x40_alphaOutlineUnvisited(f8)
+    SAutoMapperRenderState(FGetViewportSize getViewportSize, const zeus::CQuaternion& camOrientation, float camDist,
+                           float camAngle, const zeus::CVector3f& areaPoint, float drawDepth1, float drawDepth2,
+                           float alphaSurfaceVisited, float alphaOutlineVisited, float alphaSurfaceUnvisited,
+                           float alphaOutlineUnvisited)
+    : m_getViewportSize(getViewportSize)
+    , x0_viewportSize(getViewportSize())
+    , x8_camOrientation(camOrientation)
+    , x18_camDist(camDist)
+    , x1c_camAngle(camAngle)
+    , x20_areaPoint(areaPoint)
+    , x2c_drawDepth1(drawDepth1)
+    , x30_drawDepth2(drawDepth2)
+    , x34_alphaSurfaceVisited(alphaSurfaceVisited)
+    , x38_alphaOutlineVisited(alphaOutlineVisited)
+    , x3c_alphaSurfaceUnvisited(alphaSurfaceUnvisited)
+    , x40_alphaOutlineUnvisited(alphaOutlineUnvisited)
     , x44_viewportEase(Ease::None)
     , x48_camEase(Ease::None)
     , x4c_pointEase(Ease::None)
