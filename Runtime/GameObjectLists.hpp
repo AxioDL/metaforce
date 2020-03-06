80e2e97dc4848b0ae27dd28470fe5010c244ad0b
Lioncash <mathew1800@gmail.com>
2020-03-06 04:36:24 -0500

    CObjectList: Make IsQualified() a const member function
    None of the implementations modify object instance state, so this can be
    made const qualified.

--- Runtime/GameObjectLists.hpp
+++ Runtime/GameObjectLists.hpp
@@ -8,47 +8,47 @@ class CActorList : public CObjectList {
 public:
   CActorList();
 
-  bool IsQualified(const CEntity&) override;
+  bool IsQualified(const CEntity&) const override;
 };
 
 class CPhysicsActorList : public CObjectList {
 public:
   CPhysicsActorList();
-  bool IsQualified(const CEntity&) override;
+  bool IsQualified(const CEntity&) const override;
 };
 
 class CGameCameraList : public CObjectList {
 public:
   CGameCameraList();
-  bool IsQualified(const CEntity&) override;
+  bool IsQualified(const CEntity&) const override;
 };
 
 class CListeningAiList : public CObjectList {
 public:
   CListeningAiList();
-  bool IsQualified(const CEntity&) override;
+  bool IsQualified(const CEntity&) const override;
 };
 
 class CAiWaypointList : public CObjectList {
 public:
   CAiWaypointList();
-  bool IsQualified(const CEntity&) override;
+  bool IsQualified(const CEntity&) const override;
 };
 
 class CPlatformAndDoorList : public CObjectList {
 public:
   CPlatformAndDoorList();
 
-  bool IsQualified(const CEntity&) override;
-  bool IsDoor(const CEntity&);
-  bool IsPlatform(const CEntity&);
+  bool IsQualified(const CEntity&) const override;
+  bool IsDoor(const CEntity&) const;
+  bool IsPlatform(const CEntity&) const;
 };
 
 class CGameLightList : public CObjectList {
 public:
   CGameLightList();
 
-  bool IsQualified(const CEntity&) override;
+  bool IsQualified(const CEntity&) const override;
 };
 
 } // namespace urde
