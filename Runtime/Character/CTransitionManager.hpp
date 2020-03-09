39b561b0895e509cc431298590c109addbc57ddb
Lioncash <mathew1800@gmail.com>
2020-03-09 11:51:06 -0400

    CTransitionManager: std::move constructor parameters where applicable
    Allows calling code to move into the constructor

--- Runtime/Character/CTransitionManager.hpp
+++ Runtime/Character/CTransitionManager.hpp
@@ -15,7 +15,7 @@ class CTransitionManager {
   CAnimSysContext x0_animCtx;
 
 public:
-  CTransitionManager(const CAnimSysContext& sysCtx) : x0_animCtx(sysCtx) {}
+  explicit CTransitionManager(CAnimSysContext sysCtx) : x0_animCtx(std::move(sysCtx)) {}
   std::shared_ptr<CAnimTreeNode> GetTransitionTree(const std::shared_ptr<CAnimTreeNode>& a,
                                                    const std::shared_ptr<CAnimTreeNode>& b) const;
 };
