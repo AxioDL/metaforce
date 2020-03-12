93121c38f794b1bf4e71aad0a1a9f7f1e86f030f
Lioncash <mathew1800@gmail.com>
2020-03-12 13:59:53 -0400

    CProjectileWeaponDataFactory: Make GetGeneratorDesc() return a unique_ptr
    Same behavior, but with safer memory management.

--- Runtime/Particle/CProjectileWeaponDataFactory.hpp
+++ Runtime/Particle/CProjectileWeaponDataFactory.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <memory>
+
 #include "Runtime/CFactoryMgr.hpp"
 #include "Runtime/CToken.hpp"
 #include "Runtime/IOStreams.hpp"
@@ -7,14 +9,15 @@
 #include "Runtime/RetroTypes.hpp"
 
 namespace urde {
-class CWeaponDescription;
 class CSimplePool;
+class CWeaponDescription;
+
 class CProjectileWeaponDataFactory {
-  static CWeaponDescription* CreateGeneratorDescription(CInputStream& in, CSimplePool* resPool);
+  static std::unique_ptr<CWeaponDescription> CreateGeneratorDescription(CInputStream& in, CSimplePool* resPool);
   static bool CreateWPSM(CWeaponDescription* desc, CInputStream& in, CSimplePool* resPool);
 
 public:
-  static CWeaponDescription* GetGeneratorDesc(CInputStream& in, CSimplePool* resPool);
+  static std::unique_ptr<CWeaponDescription> GetGeneratorDesc(CInputStream& in, CSimplePool* resPool);
 };
 
 CFactoryFnReturn FProjectileWeaponDataFactory(const SObjectTag& tag, CInputStream& in, const CVParamTransfer& vparms,
