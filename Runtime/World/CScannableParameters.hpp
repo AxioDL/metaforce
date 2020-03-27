05988ce31c2969a7ead739d01233ca4583cf06d1
Lioncash <mathew1800@gmail.com>
2020-03-27 18:56:34 -0400

    CScannableParameters: Mark CScannableParameters as constexpr
    This only contains a single member, so this can trivially be marked
    constexpr.

--- Runtime/World/CScannableParameters.hpp
+++ Runtime/World/CScannableParameters.hpp
@@ -8,8 +8,8 @@ class CScannableParameters {
   CAssetId x0_scanId;
 
 public:
-  CScannableParameters() = default;
-  CScannableParameters(CAssetId id) : x0_scanId(id) {}
-  CAssetId GetScanId() const { return x0_scanId; }
+  constexpr CScannableParameters() = default;
+  constexpr explicit CScannableParameters(CAssetId id) : x0_scanId(id) {}
+  [[nodiscard]] constexpr CAssetId GetScanId() const { return x0_scanId; }
 };
 } // namespace urde
