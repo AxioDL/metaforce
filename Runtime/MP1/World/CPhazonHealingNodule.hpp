4d48febca299d307430bbd0e1e568e6153770afe
Luke Street <luke.street@encounterpc.com>
2020-04-03 03:44:42 -0400

    CPhazonHealingNodule: Implementation

--- Runtime/MP1/World/CPhazonHealingNodule.hpp
+++ Runtime/MP1/World/CPhazonHealingNodule.hpp
@@ -6,19 +6,35 @@
 namespace urde::MP1 {
 class CPhazonHealingNodule : public CPatterned {
 private:
-  int x568_ = 0;
-  u8 x56c_ = 0;
-  TUniqueId x56e_ = kInvalidUniqueId;
-  TCachedToken<CElectricDescription> x570_;
-  std::unique_ptr<CParticleElectric> x57c_; // was rstl::rc_ptr
-  CHealthInfo x580_;
-  int x588_ = 0; // not init in ctr
-  std::string x58c_;
+  int x568_active = 0;
+  bool x56c_emitting = false;
+  TUniqueId x56e_connId = kInvalidUniqueId;
+  TCachedToken<CElectricDescription> x570_electricDesc;
+  std::unique_ptr<CParticleElectric> x57c_particleElectric; // was rstl::rc_ptr
+  CHealthInfo x580_initialHealthInfo;
+  int x588_state = 0; // not init in ctr; same as CElitePirate::EState?
+  std::string x58c_actorLctr;
   // u32 x59c_;
 
 public:
   CPhazonHealingNodule(TUniqueId uid, std::string_view name, const CEntityInfo& info, const zeus::CTransform& xf,
                        CModelData&& mData, const CActorParameters& actParams, const CPatternedInfo& pInfo,
-                       CAssetId particleDescId, std::string str);
+                       CAssetId particleDescId, std::string actorLctr);
+
+  void AcceptScriptMsg(EScriptObjectMessage msg, TUniqueId uid, CStateManager& mgr) override;
+  void DoUserAnimEvent(CStateManager& mgr, const CInt32POINode& node, EUserEventType type, float dt) override;
+  void Render(const CStateManager& mgr) const override;
+  void Think(float dt, CStateManager& mgr) override;
+
+  void Death(CStateManager& mgr, const zeus::CVector3f& direction, EScriptObjectState state) override;
+  void Faint(CStateManager& mgr, EStateMsg msg, float dt) override;
+  void Growth(CStateManager& mgr, EStateMsg msg, float dt) override;
+  void KnockBack(const zeus::CVector3f& vec, CStateManager& mgr, const CDamageInfo& info, EKnockBackType type,
+                 bool inDeferred, float magnitude) override;
+  void Lurk(CStateManager& mgr, EStateMsg msg, float dt) override;
+  void Patrol(CStateManager& mgr, EStateMsg msg, float dt) override;
+
+private:
+  void UpdateParticleElectric(CStateManager& mgr);
 };
 } // namespace urde::MP1
