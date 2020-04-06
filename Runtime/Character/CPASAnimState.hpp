17c1582ba2f668d56eeb09e1dc5d0d3e3d037f45
Lioncash <mathew1800@gmail.com>
2020-04-06 13:06:29 -0400

    CPASAnimState: Make PickRandomAnimation() private
    This isn't used externally anywhere.

--- Runtime/Character/CPASAnimState.hpp
+++ Runtime/Character/CPASAnimState.hpp
@@ -20,13 +20,14 @@ class CPASAnimState {
   float ComputePercentErrorWeight(size_t idx, const CPASAnimParm& parm, CPASAnimParm::UParmValue parmVal) const;
   float ComputeAngularPercentErrorWeight(size_t idx, const CPASAnimParm& parm, CPASAnimParm::UParmValue parmVal) const;
 
+  s32 PickRandomAnimation(CRandom16& rand) const;
+
 public:
   explicit CPASAnimState(CInputStream& in);
   explicit CPASAnimState(int stateId);
   s32 GetStateId() const { return x0_id; }
   s32 GetNumAnims() const { return x14_anims.size(); }
   CPASAnimParm GetAnimParmData(s32 animId, u32 parmIdx) const;
-  s32 PickRandomAnimation(CRandom16& rand) const;
   std::pair<float, s32> FindBestAnimation(const rstl::reserved_vector<CPASAnimParm, 8>& parms, CRandom16& rand,
                                           s32 ignoreAnim) const;
 };
