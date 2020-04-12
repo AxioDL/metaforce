66f5685590ca6a43241ddff3932e30b6065db282
Lioncash <mathew1800@gmail.com>
2020-04-12 10:27:59 -0400

    CMaterialFilter: Mark skPassEverything as constexpr
    Removes any potential runtime initializers.

--- Runtime/Collision/CMaterialFilter.cpp
+++ Runtime/Collision/CMaterialFilter.cpp
@@ -1,6 +1,5 @@
 #include "Runtime/Collision/CMaterialFilter.hpp"
 
 namespace urde {
-const CMaterialFilter CMaterialFilter::skPassEverything({0x00000000FFFFFFFF}, {0},
-                                                        CMaterialFilter::EFilterType::Always);
+constexpr CMaterialFilter CMaterialFilter::skPassEverything({0x00000000FFFFFFFF}, {0}, EFilterType::Always);
 } // namespace urde
