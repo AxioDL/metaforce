55378f8c82b936e55d6c60386dac10e6e515c89c
Luke Street <luke.street@encounterpc.com>
2020-04-21 22:36:20 -0400

    Update AssetNameMap hashes

--- DataSpec/AssetMap32Download.cmake
+++ DataSpec/AssetMap32Download.cmake
@@ -1,3 +1,3 @@
 message(STATUS "32-bit asset name map not found; downloading to '${CMAKE_CURRENT_BINARY_DIR}/AssetNameMap32.bin'")
 file(DOWNLOAD "https://axiodl.com/files/AssetNameMap32.dat"
-     ${CMAKE_CURRENT_BINARY_DIR}/AssetNameMap32.bin SHOW_PROGRESS EXPECTED_HASH SHA1=e1df648c04d4caaf0821266464c672e0c337b6f0)
+     ${CMAKE_CURRENT_BINARY_DIR}/AssetNameMap32.bin SHOW_PROGRESS EXPECTED_HASH SHA1=90b4e941c192eef41c81e60314f348bc787d1336)
--- DataSpec/AssetMap64Download.cmake
+++ DataSpec/AssetMap64Download.cmake
@@ -1,3 +1,3 @@
 message(STATUS "64-bit asset name map not found; downloading to '${CMAKE_CURRENT_BINARY_DIR}/AssetNameMap64.bin'")
 file(DOWNLOAD "https://axiodl.com/files/AssetNameMap64.dat"
-        ${CMAKE_CURRENT_BINARY_DIR}/AssetNameMap64.bin SHOW_PROGRESS EXPECTED_HASH SHA1=335abe49d4848ddc7bb2d623acc76918365bc705)
+        ${CMAKE_CURRENT_BINARY_DIR}/AssetNameMap64.bin SHOW_PROGRESS EXPECTED_HASH SHA1=e49c03c9fff66adccec7af8120dda091636513e2)
