e1dfe9ff8e883340989cac4caed2b2f14bccf81f
Lioncash <mathew1800@gmail.com>
2020-04-23 20:34:03 -0400

    CGameOptionsTouchBar: Make use of [[maybe_unused]] where applicable
    Silences unused parameter warnings

--- Runtime/CGameOptionsTouchBar.cpp
+++ Runtime/CGameOptionsTouchBar.cpp
@@ -4,7 +4,8 @@ namespace urde {
 
 CGameOptionsTouchBar::EAction CGameOptionsTouchBar::PopAction() { return EAction::None; }
 void CGameOptionsTouchBar::GetSelection(int& left, int& right, int& value) { left = -1, right = -1, value = -1; }
-void CGameOptionsTouchBar::SetSelection(int left, int right, int value) {}
+void CGameOptionsTouchBar::SetSelection([[maybe_unused]] int left, [[maybe_used]] int right, [[maybe_used]] int value) {
+}
 
 #ifndef __APPLE__
 std::unique_ptr<CGameOptionsTouchBar> NewGameOptionsTouchBar() { return std::make_unique<CGameOptionsTouchBar>(); }
