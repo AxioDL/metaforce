deff9d054be94dc3373f44e3d3bc94062be10065
Lioncash <mathew1800@gmail.com>
2020-04-26 03:05:18 -0400

    CTimeProvider: Move global instance to cpp file
    This isn't used anywhere externally, so we can keep it hidden in the cpp
    file.

--- Runtime/CTimeProvider.hpp
+++ Runtime/CTimeProvider.hpp
@@ -1,9 +1,7 @@
 #pragma once
 namespace urde {
-class CTimeProvider;
 class CTimeProvider {
 public:
-  static CTimeProvider* g_currentTimeProvider;
   const float& x0_currentTime; // in seconds
   bool x4_first = true;
   CTimeProvider* x8_lastProvider = nullptr;
