9b94dd7c3aa28cdc9cb0c6881ae3d50e1379766e
Phillip Stephens <antidote.crk@gmail.com>
2020-04-26 18:47:01 -0700

    Fix compiling CGameOptionsTouchBar

--- Runtime/CGameOptionsTouchBar.cpp
+++ Runtime/CGameOptionsTouchBar.cpp
@@ -4,7 +4,7 @@ namespace urde {
 
 CGameOptionsTouchBar::EAction CGameOptionsTouchBar::PopAction() { return EAction::None; }
 void CGameOptionsTouchBar::GetSelection(int& left, int& right, int& value) { left = -1, right = -1, value = -1; }
-void CGameOptionsTouchBar::SetSelection([[maybe_unused]] int left, [[maybe_used]] int right, [[maybe_used]] int value) {
+void CGameOptionsTouchBar::SetSelection([[maybe_unused]] int left, [[maybe_unused]] int right, [[maybe_unused]] int value) {
 }
 
 #ifndef __APPLE__
