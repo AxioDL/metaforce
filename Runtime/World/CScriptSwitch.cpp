79a25cab5e6b8b16d333b6626da66dfa60e01666
Lioncash <mathew1800@gmail.com>
2020-05-07 11:48:34 -0400

    CScriptSwitch: Brace conditionals where applicable

--- Runtime/World/CScriptSwitch.cpp
+++ Runtime/World/CScriptSwitch.cpp
@@ -13,17 +13,18 @@ void CScriptSwitch::Accept(IVisitor& visitor) { visitor.Visit(this); }
 
 void CScriptSwitch::AcceptScriptMsg(EScriptObjectMessage msg, TUniqueId objId, CStateManager& mgr) {
   if (GetActive()) {
-    if (msg == EScriptObjectMessage::Open)
+    if (msg == EScriptObjectMessage::Open) {
       x34_opened = true;
-    else if (msg == EScriptObjectMessage::Close)
+    } else if (msg == EScriptObjectMessage::Close) {
       x34_opened = false;
-    else if (msg == EScriptObjectMessage::SetToZero) {
+    } else if (msg == EScriptObjectMessage::SetToZero) {
       if (x34_opened) {
         SendScriptMsgs(EScriptObjectState::Open, mgr, EScriptObjectMessage::None);
         if (x35_closeOnOpened)
           x34_opened = false;
-      } else
+      } else {
         SendScriptMsgs(EScriptObjectState::Closed, mgr, EScriptObjectMessage::None);
+      }
     }
   }
 
