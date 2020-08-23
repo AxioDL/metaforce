cadf85bc58a0d5d0b7d1db9aece6eb8d3294424e
Lioncash <mathew1800@gmail.com>
2020-08-23 15:48:38 -0400

    General: Resolve -Wcomma warnings
    Resolves compilation warnings by just making the chained statements
    their own separate statements.

--- Runtime/CGameOptionsTouchBar.cpp
+++ Runtime/CGameOptionsTouchBar.cpp
@@ -3,7 +3,13 @@
 namespace urde {
 
 CGameOptionsTouchBar::EAction CGameOptionsTouchBar::PopAction() { return EAction::None; }
-void CGameOptionsTouchBar::GetSelection(int& left, int& right, int& value) { left = -1, right = -1, value = -1; }
+
+void CGameOptionsTouchBar::GetSelection(int& left, int& right, int& value) {
+  left = -1;
+  right = -1;
+  value = -1;
+}
+
 void CGameOptionsTouchBar::SetSelection([[maybe_unused]] int left, [[maybe_unused]] int right, [[maybe_unused]] int value) {
 }
 
