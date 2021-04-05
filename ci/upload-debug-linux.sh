9a6eba238350b11ba68346ac7a750e178899ea0d
Luke Street <luke.street@encounterpc.com>
2021-04-05 15:56:58 -0400

    CI: Upload MSVC PDBs; compress ELF debug info

--- ci/upload-debug-linux.sh
+++ ci/upload-debug-linux.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -ex
 cd build/Binaries
 for f in hecl hecl-gui urde visigen; do
-  objcopy --only-keep-debug $f $f.dbg
+  objcopy --only-keep-debug --compress-debug-sections=zlib $f $f.dbg
 done
 strip -S hecl hecl-gui urde visigen
 sentry-cli upload-dif --org axiodl --project urde {hecl,hecl-gui,urde,visigen}{,.dbg} --include-sources
