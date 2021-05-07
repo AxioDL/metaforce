2012a7a713e0b3c1db20a6b88ef3f5201c47e97c
Phillip Stephens <antidote.crk@gmail.com>
2021-05-07 00:11:08 -0700

    Change mac bundle id

--- ci/build-dmg.sh
+++ ci/build-dmg.sh
@@ -7,5 +7,5 @@ for i in visigen hecl metaforce crashpad_handler; do
 done
 macdeployqt Metaforce.app -sign-for-notarization="$CODESIGN_IDENT" -no-strip
 create-dmg Metaforce.app --identity="$CODESIGN_IDENT" "$GITHUB_WORKSPACE"
-xcrun altool -t osx -f "$GITHUB_WORKSPACE"/*.dmg --primary-bundle-id com.axiodl.URDE --notarize-app \
+xcrun altool -t osx -f "$GITHUB_WORKSPACE"/*.dmg --primary-bundle-id com.axiodl.Metaforce --notarize-app \
   -u "$ASC_USERNAME" -p "$ASC_PASSWORD" --team-id "$ASC_TEAM_ID"
