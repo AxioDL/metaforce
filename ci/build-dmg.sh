12c38f57fa84e897161dedc57eedef8a8c929879
Luke Street <luke.street@encounterpc.com>
2021-06-12 12:48:20 -0400

    ci: Fix file arg in altool

--- ci/build-dmg.sh
+++ ci/build-dmg.sh
@@ -8,5 +8,5 @@ for i in visigen hecl metaforce crashpad_handler; do
 done
 macdeployqt Metaforce.app -sign-for-notarization="$CODESIGN_IDENT" -no-strip
 create-dmg Metaforce.app --identity="$CODESIGN_IDENT" .
-xcrun altool -t osx -f --primary-bundle-id com.axiodl.Metaforce --notarize-app \
-  -u "$ASC_USERNAME" -p "$ASC_PASSWORD" --team-id "$ASC_TEAM_ID" -- *.dmg
+xcrun altool -t osx -f *.dmg --primary-bundle-id com.axiodl.Metaforce \
+  --notarize-app -u "$ASC_USERNAME" -p "$ASC_PASSWORD" --team-id "$ASC_TEAM_ID"
