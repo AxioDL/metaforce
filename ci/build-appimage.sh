54330e43b641ec93e3edfea9d89aa4d9999b8640
Luke Street <luke.street@encounterpc.com>
2022-02-21 22:12:03 -0500

    ci: Build only metaforce

--- ci/build-appimage.sh
+++ ci/build-appimage.sh
@@ -4,17 +4,15 @@ shopt -s extglob
 # Get linuxdeploy
 cd "$RUNNER_WORKSPACE"
 curl -OL https://github.com/encounter/linuxdeploy/releases/download/continuous/linuxdeploy-$(uname -m).AppImage
-curl -OL https://github.com/encounter/linuxdeploy-plugin-qt/releases/download/continuous/linuxdeploy-plugin-qt-$(uname -m).AppImage
-chmod +x linuxdeploy-$(uname -m).AppImage linuxdeploy-plugin-qt-$(uname -m).AppImage
+chmod +x linuxdeploy-$(uname -m).AppImage
 
 # Build AppImage
 cd "$GITHUB_WORKSPACE"
 mkdir -p build/appdir/usr/{bin,share/{applications,icons/hicolor}}
 cp build/install/!(*.*) build/appdir/usr/bin
-cp -r metaforce-gui/platforms/freedesktop/{16x16,32x32,48x48,64x64,128x128,256x256,512x512,1024x1024} build/appdir/usr/share/icons/hicolor
-cp metaforce-gui/platforms/freedesktop/metaforce.desktop build/appdir/usr/share/applications
+cp -r Runtime/platforms/freedesktop/{16x16,32x32,48x48,64x64,128x128,256x256,512x512,1024x1024} build/appdir/usr/share/icons/hicolor
+cp Runtime/platforms/freedesktop/metaforce.desktop build/appdir/usr/share/applications
 
 cd build/install
 VERSION="$METAFORCE_VERSION" NO_STRIP=1 "$RUNNER_WORKSPACE"/linuxdeploy-$(uname -m).AppImage \
-  --appdir "$GITHUB_WORKSPACE"/build/appdir \
-  --plugin qt --output appimage
+  --appdir "$GITHUB_WORKSPACE"/build/appdir --output appimage
--- ci/build-dmg.sh
+++ ci/build-dmg.sh
@@ -2,11 +2,9 @@
 cd build/install
 # remove debug files before package
 rm -r Metaforce.app/Contents/MacOS/*.dSYM
-# order is important
-for i in visigen hecl metaforce crashpad_handler; do
+for i in metaforce crashpad_handler; do
   codesign --timestamp --options runtime -s "$CODESIGN_IDENT" Metaforce.app/Contents/MacOS/$i
 done
-macdeployqt Metaforce.app -sign-for-notarization="$CODESIGN_IDENT" -no-strip
 create-dmg Metaforce.app --identity="$CODESIGN_IDENT" .
 xcrun altool -t osx -f *.dmg --primary-bundle-id com.axiodl.Metaforce \
   --notarize-app -u "$ASC_USERNAME" -p "$ASC_PASSWORD" --team-id "$ASC_TEAM_ID"
