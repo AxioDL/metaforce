7186f5d4e562c4aa8c435ac43c92b52695be751d
Luke Street <luke.street@encounterpc.com>
2022-03-08 23:12:23 -0500

    Remove old aurora code

--- Graphics/src/shaders/fog_volume_filter/mod.rs
+++
@@ -1,3 +0,0 @@
-use crate::zeus::CColor;
-
-pub(crate) fn queue_fog_volume_filter(color: CColor, two_way: bool) {}
--- Graphics/src/shaders/fog_volume_plane/mod.rs
+++
@@ -1,5 +0,0 @@
-use cxx::CxxVector;
-
-use crate::zeus::CVector4f;
-
-pub(crate) fn queue_fog_volume_plane(verts: &CxxVector<CVector4f>, pass: u8) {}
