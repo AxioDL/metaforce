f0936a7fb033061cf6fdec399f10fb8bad6bd23d
Luke Street <luke@street.dev>
2022-06-13 17:05:28 -0400

    ci: More macOS bundle fixes

--- ci/build-dmg.sh
+++ ci/build-dmg.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -ex
 cd build/install
-for i in metaforce crashpad_handler; do
-  codesign --timestamp --options runtime -s "$CODESIGN_IDENT" metaforce.app/Contents/MacOS/$i
+for i in Metaforce crashpad_handler; do
+  codesign --timestamp --options runtime -s "$CODESIGN_IDENT" Metaforce.app/Contents/MacOS/$i
 done
-create-dmg metaforce.app --identity="$CODESIGN_IDENT" .
+create-dmg Metaforce.app --identity="$CODESIGN_IDENT" .
 xcrun altool -t osx -f *.dmg --primary-bundle-id com.axiodl.Metaforce \
   --notarize-app -u "$ASC_USERNAME" -p "$ASC_PASSWORD" --team-id "$ASC_TEAM_ID"
