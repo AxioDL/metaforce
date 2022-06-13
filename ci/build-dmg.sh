3abaf6b7341d292bb1d154981a56afd3c1a47e54
Luke Street <luke.street@encounterpc.com>
2022-06-13 19:20:09 -0400

    ci: Fix build-dmg when crashpad_handler is disabled

--- ci/build-dmg.sh
+++ ci/build-dmg.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -ex
 cd build/install
 for i in Metaforce crashpad_handler; do
-  codesign --timestamp --options runtime -s "$CODESIGN_IDENT" Metaforce.app/Contents/MacOS/$i
+  if [ -x "Metaforce.app/Contents/MacOS/$i" ]; then
+    codesign --timestamp --options runtime -s "$CODESIGN_IDENT" "Metaforce.app/Contents/MacOS/$i"
+  fi
 done
 create-dmg Metaforce.app --identity="$CODESIGN_IDENT" .
 xcrun altool -t osx -f *.dmg --primary-bundle-id com.axiodl.Metaforce \
