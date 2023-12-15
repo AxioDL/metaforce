200a2f0393ff259ba40f2765144a868b5c43ebd4
Phillip Stephens <antidote.crk@gmail.com>
2023-12-15 14:43:13 -0800

    Temporarily disable signing until we can figure out a new way

--- ci/build-dmg.sh
+++ ci/build-dmg.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -ex
 cd build/install
-for i in Metaforce crashpad_handler; do
-  if [ -x "Metaforce.app/Contents/MacOS/$i" ]; then
-    codesign --timestamp --options runtime -s "$CODESIGN_IDENT" "Metaforce.app/Contents/MacOS/$i"
-  fi
-done
-create-dmg Metaforce.app --identity="$CODESIGN_IDENT" .
-xcrun altool -t osx -f *.dmg --primary-bundle-id com.axiodl.Metaforce \
-  --notarize-app -u "$ASC_USERNAME" -p "$ASC_PASSWORD" --team-id "$ASC_TEAM_ID"
+#for i in Metaforce crashpad_handler; do
+#  if [ -x "Metaforce.app/Contents/MacOS/$i" ]; then
+#    codesign --timestamp --options runtime -s "$CODESIGN_IDENT" "Metaforce.app/Contents/MacOS/$i"
+#  fi
+#done
+create-dmg Metaforce.app . #--identity="$CODESIGN_IDENT" .
+#xcrun altool -t osx -f *.dmg --primary-bundle-id com.axiodl.Metaforce \
+#  --notarize-app -u "$ASC_USERNAME" -p "$ASC_PASSWORD" --team-id "$ASC_TEAM_ID"
