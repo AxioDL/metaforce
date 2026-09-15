2d79b63061352efe414999e4f89658e496c4f831
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2026-09-15 07:11:35 +0300

    Match & link CAnimSourceReaderBase on PAL (#487)

--- configure.py
+++ configure.py
@@ -1558,7 +1558,7 @@ config.libs = [
             Object(MatchingFor("GM8E01_00"), "Kyoto/Animation/CAnimSource.cpp"),
             Object(MatchingFor("GM8E01_00"), "Kyoto/Animation/CAnimSourceReader.cpp"),
             Object(
-                MatchingFor("GM8E01_00", "GM8E01_01"),
+                MatchingFor("GM8E01_00", "GM8E01_01", "GM8P01_00"),
                 "Kyoto/Animation/CAnimSourceReaderBase.cpp",
             ),
             Object(MatchingFor("GM8E01_00", "GM8E01_01"), "Kyoto/Animation/CAnimTreeAnimReaderContainer.cpp"),
