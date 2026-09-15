37a4fd2a213a7fd0d1fb9d86e7980c1f8c17c68f
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2026-09-15 07:11:36 +0300

    Match & link CAnimTreeDoubleChild on PAL (#492)

--- configure.py
+++ configure.py
@@ -1567,7 +1567,10 @@ config.libs = [
             ),
             Object(MatchingFor("GM8E01_00", "GM8P01_00"), "Kyoto/Animation/CAnimTreeBlend.cpp"),
             Object(MatchingFor("GM8E01_00", "GM8E01_01"), "Kyoto/Animation/CAnimTreeContinuousPhaseBlend.cpp"),
-            Object(MatchingFor("GM8E01_00", "GM8E01_01"), "Kyoto/Animation/CAnimTreeDoubleChild.cpp"),
+            Object(
+                MatchingFor("GM8E01_00", "GM8E01_01", "GM8P01_00"),
+                "Kyoto/Animation/CAnimTreeDoubleChild.cpp",
+            ),
             Object(MatchingFor("GM8E01_00", "GM8E01_01"), "Kyoto/Animation/CAnimTreeNode.cpp"),
             Object(
                 MatchingFor("GM8E01_00", "GM8E01_01"),
