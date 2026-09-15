a279fc059b0e0c88475b2891db0542eb3b58f71d
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2026-09-15 07:11:37 +0300

    Match & link CAnimTreeSingleChild on PAL (#494)

--- configure.py
+++ configure.py
@@ -1576,7 +1576,7 @@ config.libs = [
                 "Kyoto/Animation/CAnimTreeNode.cpp",
             ),
             Object(
-                MatchingFor("GM8E01_00", "GM8E01_01"),
+                MatchingFor("GM8E01_00", "GM8E01_01", "GM8P01_00"),
                 "Kyoto/Animation/CAnimTreeSingleChild.cpp",
             ),
             Object(MatchingFor("GM8E01_00", "GM8E01_01"), "Kyoto/Animation/CAnimTreeTimeScale.cpp"),
