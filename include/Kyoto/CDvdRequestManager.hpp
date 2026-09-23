0df92f960da0eafbf794bd43975779b601d9debe
Luke Street <luke@street.dev>
2026-09-22 19:54:44 -0600

    Fix duplicated CDvdRequestSys

--- include/Kyoto/CDvdRequestManager.hpp
+++ include/Kyoto/CDvdRequestManager.hpp
@@ -3,8 +3,19 @@
 
 class CDvdRequestSys {
 public:
+  CDvdRequestSys() {
+    if (mManagerInstalled != true) {
+      mManagerInstalled = true;
+    }
+  }
+  ~CDvdRequestSys() {
+    if (mManagerInstalled == true) {
+      mManagerInstalled = false;
+    }
+  }
+
+private:
   static bool mManagerInstalled;
 };
-
 
 #endif // _CDVDREQUESTMANAGER
